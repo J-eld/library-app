0aed81604cec84c6d80c3f1734098d19dacfbfaf
J-eld <70251176+J-eld@users.noreply.github.com>
2021-12-21 23:30:58 +1300

    added Header component

---
+++ frontend/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "../styles/Header.module.css";
+import Book from "../assets/book.png";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className={styles.root}>
+      <div className={styles.title}>
+        <Image src={Book} />
+        <h1 className={styles.titleText}>Library App</h1>
+      </div>
+      <div className={styles.loginSignupButtons}>
+        <Link href="/login">
+          <button className="btn">Login</button>
+        </Link>
+        <Link href="/register">
+          <button className="btn">Register</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
