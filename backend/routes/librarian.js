8eb56df8e8543d74aaf27b95b853d95e6b481642
J-eld <70251176+J-eld@users.noreply.github.com>
2021-12-21 20:17:31 +1300

    created librarian route and endpoints

---
+++ backend/routes/librarian.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { BorrowedBooks } = require("../models/borrowed_books");
+const router = express.Router();
+
+router.delete("/removeBookFromList/:book_id", (req, res) => {
+  const book_id = req.params.book_id;
+
+  BorrowedBooks.deleteOne({ id: book_id }, (err, result) => {
+    if (err)
+      return res.send({
+        status: 500,
+        message:
+          "An error occurred when attempting to remove the book from the borrowed book list",
+      });
+
+    if (!result.deletedCount)
+      return res.send({ status: 404, message: "The book was not found" });
+
+    res.send({
+      status: 200,
+      message: "The book was successfully removed from the borrowed list",
+    });
+  });
+});
+
+module.exports = router;
