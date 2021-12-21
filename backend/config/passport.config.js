f29adbefe1d76c14e674affeed5d9991539f37ad
J-eld <70251176+J-eld@users.noreply.github.com>
2021-12-21 21:06:29 +1300

    changed method of exporting passport

--- backend/config/passport.config.js
+++ backend/config/passport.config.js
@@ -3,60 +3,60 @@ const { Librarians } = require("../models/librarians");
 const bcrypt = require("bcrypt");
 const localStrategy = require("passport-local").Strategy;
 
-module.exports = function (passport) {
-  passport.use(
-    new localStrategy(async (email, password, role, done) => {
-      if (role === "librarian") {
-        Librarians.findOne({ email: email }, (err, user) => {
-          if (err) throw err;
-          if (!user) return done(null, false);
-
-          const isPasswordCorrect = bcrypt.compareSync(password, user.password);
-
-          return isPasswordCorrect ? done(null, user) : done(null, false);
-        });
-      } else if (role === "bookworm") {
-        Bookworms.findOne({ email: email }, (err, user) => {
-          if (err) throw err;
-          if (!user) return done(null, false);
-
-          const isPasswordCorrect = bcrypt.compareSync(password, user.password);
-
-          return isPasswordCorrect ? done(null, user) : done(null, false);
-        });
-      } else {
-        return done(null, false);
-      }
-    })
-  );
-
-  passport.serializeUser((user, done) => {
-    done(null, { id: user.id, role: user.role });
-  });
-
-  passport.deserializeUser((id, done) => {
+passport.use(
+  new localStrategy(async (email, password, role, done) => {
     if (role === "librarian") {
-      Librarians.findOne({ _id: id }, (err, user) => {
-        const userInfo = {
-          id: user._id,
-          email: user.email,
-          firstname: user.firstname,
-          lastname: user.lastname,
-        };
-        done(err, userInfo);
+      Librarians.findOne({ email: email }, (err, user) => {
+        if (err) throw err;
+        if (!user) return done(null, false);
+
+        const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+
+        return isPasswordCorrect ? done(null, user) : done(null, false);
       });
     } else if (role === "bookworm") {
-      Bookworms.findOne({ _id: id }, (err, user) => {
-        const userInfo = {
-          id: user._id,
-          email: user.email,
-          firstname: user.firstname,
-          lastname: user.lastname,
-        };
-        done(err, userInfo);
+      Bookworms.findOne({ email: email }, (err, user) => {
+        if (err) throw err;
+        if (!user) return done(null, false);
+
+        const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+
+        return isPasswordCorrect ? done(null, user) : done(null, false);
       });
     } else {
-      done(null, false);
+      return done(null, false);
     }
-  });
-};
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, { id: user.id, role: user.role });
+});
+
+passport.deserializeUser((id, done) => {
+  if (role === "librarian") {
+    Librarians.findOne({ _id: id }, (err, user) => {
+      const userInfo = {
+        id: user._id,
+        email: user.email,
+        firstname: user.firstname,
+        lastname: user.lastname,
+      };
+      done(err, userInfo);
+    });
+  } else if (role === "bookworm") {
+    Bookworms.findOne({ _id: id }, (err, user) => {
+      const userInfo = {
+        id: user._id,
+        email: user.email,
+        firstname: user.firstname,
+        lastname: user.lastname,
+      };
+      done(err, userInfo);
+    });
+  } else {
+    done(null, false);
+  }
+});
+
+module.exports = passport;
