e4d174cc235d50131dcfefc0b0e467abb37e3532
J-eld <70251176+J-eld@users.noreply.github.com>
2021-12-21 19:18:03 +1300

    added middleware

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
+// initialise express instance
 const app = express();
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// routes
 app.get("/", (req, res) => {
   res.send("hello world");
 });
